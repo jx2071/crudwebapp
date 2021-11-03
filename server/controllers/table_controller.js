b547e2d126d83d55d1699cfb7dd3b06d05b59820
Alex X <alex@MacBook-Air.local>
2021-11-03 02:15:44 -0400

    Second Commit

--- server/controllers/table_controller.js
+++ server/controllers/table_controller.js
@@ -17,8 +17,8 @@ exports.view = (req, res) => {
     pool.getConnection((err, con) => {
         if (err) throw err; // Failed to connect
         console.log('Connected to Database');
-
-        con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
+        var table_name = process.env.TABLE_NAME;
+        con.query(`SELECT * FROM ${table_name}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
             // When query is done, release the connection
             con.release();
             console.log('Database Connection Released');
@@ -29,7 +29,7 @@ exports.view = (req, res) => {
                 column_name.push(fields[key]["name"]);
             });
 
-            res.render('home', { result, column_name });
+            res.render('home', { result, column_name, table_name, helpers: { inc: function (value) { return value + 1; } } });
         })
     })
 }
@@ -39,7 +39,6 @@ exports.find = (req, res) => {
     pool.getConnection((err, con) => {
         if (err) throw err; // Failed to connect
         console.log('Connected to Database');
-
 
         let searchTerm = req.body.search;
         con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
@@ -60,6 +59,7 @@ exports.find = (req, res) => {
             }
             searchSQL = searchSQL.substring(0, searchSQL.length - 3);
 
+            //TODO Add where statemtn from env
             con.query(`SELECT * FROM ${process.env.TABLE_NAME} WHERE (${searchSQL})`, LIKE, (error, result, fields) => {
                 con.release();
                 console.log('Database Connection Released');
@@ -76,7 +76,6 @@ exports.form = (req, res) => {
         console.log('Connected to Database');
 
         con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
-            // When query is done, release the connection
             con.release();
             console.log('Database Connection Released');
             if (error) throw error;
@@ -121,10 +120,84 @@ exports.insert = (req, res) => {
                 con.release();
                 console.log('Database Connection Released');
                 if (error) throw error;
-                console.log('Success');
+                console.log('Record Insert Success');
                 res.render('add_record', { alert: 'Record added successfully!' });
             })
 
+        })
+    })
+}
+
+exports.edit = (req, res) => {
+    pool.getConnection((err, con) => {
+        if (err) throw err; // Failed to connect
+        console.log('Connected to Database');
+
+        con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
+            con.release();
+            console.log('Database Connection Released');
+            if (error) throw error;
+
+            var column_name = [];
+            Object.keys(fields).forEach(key => {
+                column_name.push(fields[key]["name"]);
+            });
+
+            var index = parseInt(req.params.id) - 1;
+            var instance = result[index];
+            var body = `<form class="row g-3 needs-validation" method="POST" action="/editrecord/${req.params.id}" novalidate>`;
+            for (let x in column_name) {
+                body += `<div class="form-floating"><input type="text" class="form-control" id="floatingInputValue" placeholder=" " value="${instance[column_name[x]]}" name=${parseInt(x) + 1}><label for="floatingInputValue"> ${column_name[x]}</label></div>`
+            }
+            res.render('edit_record', { body });
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    pool.getConnection((err, con) => {
+        if (err) throw err; // Failed to connect
+        console.log('Connected to Database');
+
+        con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
+            if (error) throw error;
+
+            var column_name = [];
+            Object.keys(fields).forEach(key => {
+                column_name.push(fields[key]["name"]);
+            });
+            var y = 1;
+            var VALUES = [];
+            var updateSQL = `UPDATE ${process.env.TABLE_NAME} SET `;
+            for (let x in column_name) {
+                updateSQL += `${column_name[x]} = ?, `;
+                VALUES.push(req.body[y]);
+                y++;
+            }
+            updateSQL = updateSQL.substring(0, updateSQL.length - 2);
+            updateSQL += ` WHERE ${column_name[0]} = ${req.body[1]}`;
+            con.query(updateSQL, VALUES, (err, result) => {
+                if (error) throw error;
+                console.log('Record Update Success');
+                con.query(`SELECT * FROM ${process.env.TABLE_NAME}${process.env.WHERE_STATEMENT}`, (error, result, fields) => {
+                    con.release();
+                    console.log('Database Connection Released');
+                    if (error) throw error;
+
+                    var column_name = [];
+                    Object.keys(fields).forEach(key => {
+                        column_name.push(fields[key]["name"]);
+                    });
+
+                    var index = parseInt(req.params.id) - 1;
+                    var instance = result[index];
+                    var body = `<form class="row g-3 needs-validation" method="POST" action="/editrecord/${req.params.id}" novalidate>`;
+                    for (let x in column_name) {
+                        body += `<div class="row mb-3"><label class="col-sm-4 col-form-label">${column_name[x]}</label><div class="col-sm-7"><input type="text" class="form-control" value="${instance[column_name[x]]}" name=${parseInt(x) + 1}></div></div>`
+                    }
+                    res.render('edit_record', { body, alert: `Record ${column_name[0]} ${instance[column_name[0]]} updated successfully!` });
+                })
+            })
         })
     })
 }
--- server/routes/table.js
+++ server/routes/table.js
@@ -6,5 +6,7 @@ router.get('/', tableController.view);
 router.post('/', tableController.find);
 router.get('/addrecord', tableController.form);
 router.post('/addrecord', tableController.insert);
+router.get('/editrecord/:id', tableController.edit);
+router.post('/editrecord/:id', tableController.update);
 
 module.exports = router;
